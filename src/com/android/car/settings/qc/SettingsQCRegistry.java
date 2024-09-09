5f322900cccdd57917b9eeea6b6e26ea6f7e70d3
cassieyw <cassieyw@google.com>
2024-09-09 13:54:30 -0700

    Add content provider to enable layout bounds for the debug panel
    Flag: EXEMPT internal testing tool, only available on userdebug builds
    Bug: 362800323
    Bug: 372301141
    Test: manual
    Change-Id: I50dc517fb871cb6de532e712e8c773fc9b4609f6
    (cherry picked from commit 6aab63deff030267a68876843a95f5e70da920e0)

--- src/com/android/car/settings/qc/SettingsQCRegistry.java
+++ src/com/android/car/settings/qc/SettingsQCRegistry.java
@@ -126,10 +126,17 @@ public class SettingsQCRegistry {
             .authority(AUTHORITY)
             .appendPath("navigation_volume_slider")
             .build();
+
     public static final Uri DEBUG_DRIVING_MODE_URI = new Uri.Builder()
             .scheme(ContentResolver.SCHEME_CONTENT)
             .authority(AUTHORITY)
             .appendPath("debug_driving_mode_toggle")
+            .build();
+
+    public static final Uri DEBUG_LAYOUT_BOUNDS_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .appendPath("debug_layout_bounds_toggle")
             .build();
 
     // End Uris
@@ -157,6 +164,7 @@ public class SettingsQCRegistry {
         map.put(CALL_VOLUME_SLIDER_URI, CallVolumeSlider.class);
         map.put(NAVIGATION_VOLUME_SLIDER_URI, NavigationVolumeSlider.class);
         map.put(DEBUG_DRIVING_MODE_URI, DebugDriveModeRow.class);
+        map.put(DEBUG_LAYOUT_BOUNDS_URI, DebugLayoutBoundsRow.class);
 
         return map;
     }
