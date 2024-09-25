ea91849f60a7cab1ed64336e3f7b5cfbcc1177ef
cassieyw <cassieyw@google.com>
2024-09-24 23:09:01 -0700

    Add a provider to switch drive/park mode for the debug panel
    Flag: EXEMPT internal testing tool, only available on userdebug builds
    Bug: 362800323
    Bug: 372301141
    Test: manual
    Change-Id: I8b3987bd6fb220a4fb824e31874e07cbc390f99b
    (cherry picked from commit f0ba9ae4a551b462c354e8505b6b6e6920ebf021)

--- src/com/android/car/settings/qc/SettingsQCRegistry.java
+++ src/com/android/car/settings/qc/SettingsQCRegistry.java
@@ -126,6 +126,12 @@ public class SettingsQCRegistry {
             .authority(AUTHORITY)
             .appendPath("navigation_volume_slider")
             .build();
+    public static final Uri DEBUG_DRIVING_MODE_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .appendPath("debug_driving_mode_toggle")
+            .build();
+
     // End Uris
 
     @VisibleForTesting
@@ -150,6 +156,7 @@ public class SettingsQCRegistry {
         map.put(MEDIA_VOLUME_SLIDER_WITHOUT_ICON_URI, MediaVolumeSliderWithoutIcon.class);
         map.put(CALL_VOLUME_SLIDER_URI, CallVolumeSlider.class);
         map.put(NAVIGATION_VOLUME_SLIDER_URI, NavigationVolumeSlider.class);
+        map.put(DEBUG_DRIVING_MODE_URI, DebugDriveModeRow.class);
 
         return map;
     }
